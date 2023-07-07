01a849892b551d6f63f886f2000deb3d7f392713
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-07-07 16:55:07 +0330

    the game completed

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const secretPhrases=["dog","cat","that","you","never"];
 let randomItem="";
 let clicked=[];
 let result="";
+let mistakes = 0;
 
 function selectRandomItem(){
     randomItem = secretPhrases[Math.floor(Math.random() * secretPhrases.length)];
@@ -12,16 +13,27 @@ function selectRandomItem(){
 
 function setUnderScors(){
     let spletedWord=randomItem.split("");
-    let mappedWord=spletedWord.map(letter => clicked.indexOf(letter) >= 0 ? letter : "_");
-    result = mappedWord.join();
-    document.getElementById("clue").querySelector("p").innerText=result;
+    let mappedWord=spletedWord.map(letter => (clicked.indexOf(letter) >= 0 ? letter : "_"));
+    result = mappedWord.join("");
+     document.getElementById("clue").querySelector("p").innerText=result;
+    
 }
 
 function checkIfWon(){
-  if ( randomItem === result){
-    document.getElementById("gameover").querySelector("p").style="block";
+  if (randomItem === result){
+    document.getElementById("gameover").querySelector("p").style.display="block";
     document.getElementById("image").querySelector("img").src="assests/winner.png";
   }
+}
+function checkIfLost(){
+  if(mistakes === 6){
+  document.getElementById("gameover").querySelector("p").style.display="block";
+  document.getElementById("clue").querySelector("p").innerText=`Random Word Is: ${randomItem}`;
+}
+}
+function updateHangmanPicture(){
+ const image= document.getElementById("image").querySelector("img");
+image.src=`assests/hangman${mistakes}.png`;
 }
 
 function letterHandler(letter){
@@ -34,21 +46,18 @@ function letterHandler(letter){
             checkIfWon();
 
     }else if(randomItem.indexOf(letter) === -1){
-
+        mistakes++;
+        checkIfLost();
+        updateHangmanPicture();
     }
 }
 
 function buttonHandler(event){
-    letterHandler(event.target.id);
-    
+    letterHandler(event.target.id);  
 }
-
-
-
-
-
-
-
+function keyHandler(event){
+  letterHandler(event.key);
+}
 
 selectRandomItem();
 setUnderScors();
